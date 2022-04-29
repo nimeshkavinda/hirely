735f41dbe307ce7b7e0cf6c366f03db14f1ca822
Nimesh Kavinda <nimeshkavinda13@gmail.com>
2022-04-29 23:05:06 +0530

    edit job

--- src/app/JobDetails/EditJob/EditJob.js
+++ src/app/JobDetails/EditJob/EditJob.js
@@ -1,6 +1,8 @@
 import classNames from "./EditJob.module.scss";
 import Header from "../../../components/Header/Header";
 import Footer from "../../../components/Footer/Footer";
+import { Form, Input, Button, Select, Checkbox } from "antd";
+import { BsPlus } from "react-icons/bs";
 
 const EditJob = () => {
   return (
@@ -9,7 +11,45 @@ const EditJob = () => {
         <Header />
       </div>
       <div className={classNames.heading}>Edit job</div>
-      <div className={classNames.wrapper}></div>
+      <div className={classNames.formWrapper}>
+        <Form layout="vertical">
+          <Form.Item label="Job title">
+            <Input />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item
+              label="Industry"
+              style={{ display: "inline-block", width: "49%", float: "left" }}
+            >
+              <Select />
+            </Form.Item>
+            <Form.Item
+              label="Job type"
+              style={{ display: "inline-block", width: "49%", float: "right" }}
+            >
+              <Select />
+            </Form.Item>
+          </Form.Item>
+          <Form.Item label="Location">
+            <Select />
+          </Form.Item>
+          <Form.Item label="Salary">
+            <Select />
+          </Form.Item>
+          <Form.Item label="Job description">
+            <Input.TextArea showCount maxLength={2500} />
+          </Form.Item>
+          <Form.Item>
+            <Checkbox>This is a remote job</Checkbox>
+          </Form.Item>
+          <Form.Item className={classNames.buttonWrapper}>
+            <Button type="primary">Update job</Button>
+            <Button type="primary" danger>
+              Delete job
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
       <Footer />
     </>
   );
