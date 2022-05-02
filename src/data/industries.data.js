47ab35c023d06670b1cba356164d856cba47ae4d
Nimesh Kavinda <nimeshkavinda13@gmail.com>
2022-05-02 15:19:48 +0530

    add data

---
+++ src/data/industries.data.js
@@ -0,0 +1,44 @@
+const industries = [
+  {
+    id: 1,
+    name: "Energy & resources",
+    value: "energy-resources",
+  },
+  {
+    id: 2,
+    name: "Agriculture",
+    value: "agriculture",
+  },
+  {
+    id: 3,
+    name: "Health care",
+    value: "health-care",
+  },
+  {
+    id: 4,
+    name: "Banking & finance",
+    value: "banking-finance",
+  },
+  {
+    id: 5,
+    name: "Information technology",
+    value: "information-technology",
+  },
+  {
+    id: 6,
+    name: "Marketing",
+    value: "marketing",
+  },
+  {
+    id: 7,
+    name: "Education",
+    value: "education",
+  },
+  {
+    id: 8,
+    name: "Hospitality & tourism",
+    value: "hospitality-tourism",
+  },
+];
+
+export default industries;
---
+++ src/data/jobTypes.data.js
@@ -0,0 +1,24 @@
+const jobTypes = [
+  {
+    id: 1,
+    name: "Full-time",
+    value: "full-time",
+  },
+  {
+    id: 2,
+    name: "Part-time",
+    value: "part-time",
+  },
+  {
+    id: 3,
+    name: "Contract",
+    value: "contract",
+  },
+  {
+    id: 4,
+    name: "Internship",
+    value: "internship",
+  },
+];
+
+export default jobTypes;
