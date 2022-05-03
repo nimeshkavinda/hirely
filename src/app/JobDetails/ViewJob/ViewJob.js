c630c846c3b7726b5e7518601bdb314b733d10d8
Nimesh Kavinda <nimeshkavinda13@gmail.com>
2022-05-03 16:02:35 +0530

    add apply job logic

--- src/app/JobDetails/ViewJob/ViewJob.js
+++ src/app/JobDetails/ViewJob/ViewJob.js
@@ -1,20 +1,30 @@
 import classNames from "./ViewJob.module.scss";
-import { Image, Button, Spin } from "antd";
+import { Image, Button, Spin, message } from "antd";
 import { BsDot } from "react-icons/bs";
 import { FaBriefcase, FaGlobeAmericas } from "react-icons/fa";
 import { HiOfficeBuilding } from "react-icons/hi";
 // import Header from "../../../components/Header/Header";
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ac from "../../../redux/actions";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const ViewJob = () => {
   const urlParams = useParams();
+  const navigation = useNavigate();
   const dispatch = useDispatch();
+  const [uid, setUid] = useState(localStorage.getItem("uid"));
+  const [userType, setUserType] = useState(localStorage.getItem("userType"));
+  const [applyStatus, setApplyStatus] = useState(false);
 
   useEffect(() => {
     dispatch(ac.getJobById(urlParams.id));
+    setUid(localStorage.getItem("uid"));
+    setUserType(localStorage.getItem("userType"));
+    if (localStorage.getItem("userType") === "candidate") {
+      dispatch(ac.getCandidateByUid(localStorage.getItem("uid")));
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -26,10 +36,66 @@ const ViewJob = () => {
     return fetching;
   });
 
+  // const getCandidateByUid = useSelector(
+  //   ({ getCandidateByUid }) => getCandidateByUid
+  // );
+
+  const getCandidateByUidFetching = useSelector(
+    ({ getCandidateByUid: { fetching } }) => {
+      return fetching;
+    }
+  );
+
+  const updateJob = useSelector(({ updateJob }) =>
+    updateJob.data ? updateJob.data : {}
+  );
+
+  const updateJobFetching = useSelector(({ updateJob: { fetching } }) => {
+    return fetching;
+  });
+
+  const onApply = () => {
+    dispatch(
+      ac.updateJob(urlParams.id, {
+        ...getJobById,
+        candidates: [
+          ...getJobById.candidates,
+          {
+            uid: uid,
+          },
+        ],
+      })
+    );
+  };
+
+  useEffect(() => {
+    if (updateJob.status === "success") {
+      let isFound = updateJob.data.candidates.some((candidate) => {
+        if (candidate.uid === uid) {
+          return true;
+        }
+        return false;
+      });
+      console.log(isFound);
+      if (isFound) {
+        setApplyStatus(true);
+        message.success("Successfully applied");
+      } else {
+        message.error("Failed to apply for job");
+      }
+    }
+    if (updateJob.error) {
+      message.error("Failed to apply for job");
+    }
+  }, [updateJob, uid, updateJobFetching, dispatch]);
+
   return (
     <div className={classNames.wrapper}>
       {/* <Header /> */}
-      <Spin size="large" spinning={getJobByIdFetching}>
+      <Spin
+        size="large"
+        spinning={getJobByIdFetching || getCandidateByUidFetching}
+      >
         <div className={classNames.jobWrapper}>
           <div className={classNames.header}>
             <Image
@@ -60,7 +126,23 @@ const ViewJob = () => {
           </div>
           <div className={classNames.salary}>LKR {getJobById?.salary}</div>
           <div className={classNames.buttonWrapper}>
-            <Button type="primary">Apply for this job</Button>
+            {userType === "employer" ? (
+              <Button
+                type="primary"
+                onClick={() => navigation(`/job/${urlParams.id}/edit`)}
+              >
+                Edit job
+              </Button>
+            ) : (
+              <Button
+                type="primary"
+                onClick={onApply}
+                loading={updateJobFetching || getCandidateByUidFetching}
+                disabled={applyStatus}
+              >
+                {applyStatus ? "Already applied" : "Apply for this job"}
+              </Button>
+            )}
           </div>
           <div className={classNames.description}>
             {getJobById?.description}
